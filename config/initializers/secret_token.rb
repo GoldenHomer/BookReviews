8c55a64f242c1c8ae16b1aa965cc84c1e63dc5ac
Jose Contreras <jocontre@joselcontreras.com>
2014-08-02 01:16:59 -0700

    Skeleton for app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookReviews::Application.config.secret_token = '6a77a5b4cdb6977d9bd8741f24f72d828c8da6d1214d868a4090addfc2d656e477ae2a69fdb427a435df8267b403be16aa12f3a2ecd5c1981834c50b7fd8ce13'
